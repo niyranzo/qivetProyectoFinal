9d4538fdfcedc7e8819c94c01d106d44ef0b3259
niyranzo <yranzo.nicole@gmail.com>
2025-06-02 13:40:54 +0200

    railway cambios

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,21 +4,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'qivet_db',
-  process.env.DB_USER || 'admin',
-  process.env.DB_PASSWORD || 'admin',
-  {
-    host: process.env.DB_HOST || 'postgres',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST, // <--- AHORA SÓLO CONFÍA EN LA VARIABLE DE ENTORNO
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Puede ser necesario para Railway
+            }
+        }
     }
-  }
 );
 
 // Función para probar la conexión
