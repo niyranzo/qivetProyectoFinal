d0da76af993930d1250095bc83f3fd2952fa3643
niyranzo <yranzo.nicole@gmail.com>
2025-06-02 13:49:36 +0200

    railway cambios

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,29 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST, // <--- AHORA SÓLO CONFÍA EN LA VARIABLE DE ENTORNO
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // Puede ser necesario para Railway
-            }
-        }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-);
+  }
+});
 
 // Función para probar la conexión
 const testConnection = async () => {
